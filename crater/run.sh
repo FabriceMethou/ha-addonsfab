9506a942d2d20eac1656407e93852f5540ab67a1
CharlB <git@charlb.fr>
2024-07-27 18:19:15 +0200

    Dp symlink at startup to make storage persistant

--- crater/run.sh
+++ crater/run.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/with-contenv bashio
-touch /data/database.sqlite
-chown $user:$user /data/database.sqlite
-chmod 775 /data/database.sqlite
+
+# /var/www/storage/app/database_created is created once setup is finished
+# but more generally, we have to keep /var/www/storage/ persistent
+# we put it in /data/crater/storage
+
+
+ISCREATED_FILE="/data/crater/storage/app/database_created"
+
+# Check if the file exists
+if [ -f "$ISCREATED_FILE" ]; then
+    echo "Database already exists, using exsiting one"
+    rm -rf /var/www/storage
+    ln -s /data/crater/storage /var/www/storage
+else
+    echo "Database does not already exists, creating an empty one"
+    mkdir -p /data/crater/storage
+    chmod 777 -R /data/crater/storage
+    rm -rf /var/www/storage
+    ln -s /data/crater/storage /var/www/storage
+
+    touch /data/crater/database.sqlite
+    chmod 777 /data/crater/database.sqlite
+fi
+
 id
 nginx
 php-fpm
